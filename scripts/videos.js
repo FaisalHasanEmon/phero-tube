0da308ea3cbcca2ced5093e636b57a90eb73393c
FaisalHasanEmon <faisalhasan5833@gmail.com>
2024-10-10 22:59:47 +0600

    video section added using api

--- scripts/videos.js
+++ scripts/videos.js
@@ -1,22 +1,26 @@
 // 1. fetch, load and display
 
-// Loading Data
+// Loading Categories
 const loadCategories = () => {
   fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
     .then((res) => res.json())
     .then((data) => displayCategories(data.categories))
     .catch((error) => console.error("Failed to load data", error));
 };
+// Loading Videos
+const loadVideos = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+    .then((res) => res.json())
+    .then((data) => displayVideos(data.videos))
+    .catch((error) => console.error("Failed to load videos", error));
+};
 
-// category : "Music"
-// category_id : "1001"
-// [[Prototype]] : Object
-
+// Display Categories
 const displayCategories = (categories) => {
   const categoryContainer = document.getElementById("categories");
 
   categories.forEach((item) => {
-    console.log(item);
+    // console.log(item);
     // Create a button
     const button = document.createElement("button");
     button.classList = "btn";
@@ -27,4 +31,50 @@ const displayCategories = (categories) => {
   });
 };
 
+const demoObj = {
+  category_id: "1001",
+  video_id: "aaab",
+  thumbnail: "https://i.ibb.co/QPNzYVy/moonlight.jpg",
+  title: "Midnight Serenade",
+  authors: [
+    {
+      profile_picture: "https://i.ibb.co/fDbPv7h/Noha.jpg",
+      profile_name: "Noah Walker",
+      verified: false,
+    },
+  ],
+  others: {
+    views: "543K",
+    posted_date: "",
+  },
+  description:
+    "'Midnight Serenade' by Noah Walker is a soulful journey into the depths of the night, capturing the mystique and allure of a moonlit evening. With 543K views, this song brings together tender melodies and evocative lyrics, making it a favorite among listeners seeking a contemplative yet uplifting experience. Immerse yourself in this musical masterpiece and feel the calm embrace of the night.",
+};
+
+// Display Videosf
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("videos");
+  videos.forEach((video) => {
+    console.log(video);
+    const card = document.createElement("div");
+    card.classList = "card card-compact";
+    card.innerHTML = `
+        <figure>
+            <img
+            src=${video.thumbnail}
+            alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">Shoes!</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+            </div>
+        </div>
+    `;
+    videoContainer.append(card);
+  });
+};
+
 loadCategories();
+loadVideos();
